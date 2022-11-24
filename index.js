d20d264732aab126627f7a5a62ec2047239949ac
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-11-24 09:12:20 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+let blocks;
+let feet;
+let fare;
+
+function distanceFromHqInBlocks (distance) {
+  if (distance > 42) {
+    let blocks = distance % 42
+    return blocks
+  } else {
+    let blocks = 42 - distance
+    return blocks
+  }
+}
+
+function distanceFromHqInFeet (distance) {
+  if (distance > 42) {
+    let feet = ((distance - 42) * 264)
+    return feet
+  } else {
+    let feet = ((42 - distance) * 264)
+    return feet
+  }
+}
